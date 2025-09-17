0f933c9a6042a815705d51617e32e0e860f5b2c7
harshitboat <harshitsinghgla2415@gmail.com>
2025-09-17 22:29:38 +0530

    day1

---
+++ method_call.java
@@ -0,0 +1,13 @@
+public class method_call{
+
+    public void printMessage(){
+        System.out.print("hello,world");
+    }
+
+    public static void main(String[]args){
+        method_call obj=new method_call();
+
+        obj.printMessage();
+
+    }
+}
