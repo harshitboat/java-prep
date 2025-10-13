9c874ffdc527e72814277ca2692c72bd0ba3da79
harshit <harshitsinghgla2415@gmail.com>
2025-10-13 09:18:47 +0530

    functional interface square

---
+++ LambdaSquare.java
@@ -0,0 +1,16 @@
+@FunctionalInterface
+interface SquareCal {
+    int square(int x);
+}
+
+public class LambdaSquare {
+    public static void main(String[] args) {
+        SquareCal sq = (n) -> n * n;
+
+        int num1 = 5;
+        int num2 = 7;
+
+        System.out.println("Square of " + num1 + ": " + sq.square(num1));
+        System.out.println("Square of " + num2 + ": " + sq.square(num2));
+    }
+}
